dee78c0f7042d5b71ed7ccf20bc3b26a47b9fd64
cg <cg2100773258@163.com>
2023-07-23 23:18:53 +0800

    提交

--- qi_blog_con_after/src/main/java/com/cg/Main.java
+++ qi_blog_con_after/src/main/java/com/cg/Main.java
@@ -1,4 +1,6 @@
-package com.cg;public class Main {
+package com.cg;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
--- qi_blog_front_after/src/main/java/com/front/cg/FrontApplication.java
+++ qi_blog_front_after/src/main/java/com/front/cg/FrontApplication.java
@@ -1,7 +1,11 @@
 package com.front.cg;
 
-public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(FrontApplication.class,args);
     }
 }
